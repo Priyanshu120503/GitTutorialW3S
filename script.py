dc4490de8c3ea7b1c7ac9e969b1619ddded2ce69
Priyanshu120503 <imrathore1205@gmail.com>
2022-10-04 22:53:50 +0530

    Updated script.py to find the prime factors of the number given by user

--- script.py
+++ script.py
@@ -5,3 +5,12 @@ print()
 
 print('\n'.join(['  '*(n-i) + '* '*i for i in range(1,n+1)]))
 
+f = []
+i = 2
+num = n
+while n!=1:
+  if n%i==0:
+    f.append(i)
+    n //= i
+  else: i+=1
+print(f'Prime Factors of {num} are: {f}') 
